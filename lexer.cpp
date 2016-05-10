5fed628dba080b504c4fba396a8ab6b507f094c7
Pawel Jankowski <pjankow6@gmail.com>
2016-05-10 18:31:21 +0200

    added registers and instructions for automata

--- lexer.cpp
+++ lexer.cpp
@@ -11,12 +11,13 @@
 enum Type
 {
     ADDRESS = 1,
-    OPCODE,
+    INSTRUCTION,
     HEX_NUMBER,
     REGISTER,
     COMMENT,
     OFFSET,
-    MEMORY_ACCESS
+    MEMORY_ACCESS,
+    HEX_OR_INST,
 };
 
 Options options;
@@ -24,29 +25,280 @@ Automata automata; //[0-9a-f]+:?
 
 void init_automata()
 {
-    std::map < char, int > state0, state1;
-    state0['0'] = 0;
-    state0['1'] = 0;
-    state0['2'] = 0;
-    state0['3'] = 0;
-    state0['4'] = 0;
-    state0['5'] = 0;
-    state0['6'] = 0;
-    state0['7'] = 0;
-    state0['8'] = 0;
-    state0['9'] = 0;
-    state0['a'] = 0;
-    state0['b'] = 0;
-    state0['c'] = 0;
-    state0['d'] = 0;
-    state0['e'] = 0;
-    state0['f'] = 0;
-    state0[':'] = 1;
-    automata.push(state0);
-    automata.push(state1);
+    int states_count = 12;
+    std::map < char, int > states[states_count];
+    states[0]['0'] = 1;
+    states[0]['1'] = 1;
+    states[0]['2'] = 1;
+    states[0]['3'] = 1;
+    states[0]['4'] = 1;
+    states[0]['5'] = 1;
+    states[0]['6'] = 1;
+    states[0]['7'] = 1;
+    states[0]['8'] = 1;
+    states[0]['9'] = 1;
+    states[0]['a'] = 0;
+    states[0]['b'] = 0;
+    states[0]['c'] = 0;
+    states[0]['d'] = 0;
+    states[0]['e'] = 0;
+    states[0]['f'] = 0;
+    states[0]['g'] = 10;
+    states[0]['h'] = 10;
+    states[0]['i'] = 10;
+    states[0]['j'] = 10;
+    states[0]['k'] = 10;
+    states[0]['l'] = 10;
+    states[0]['m'] = 10;
+    states[0]['n'] = 10;
+    states[0]['o'] = 10;
+    states[0]['p'] = 10;
+    states[0]['q'] = 10;
+    states[0]['r'] = 10;
+    states[0]['s'] = 10;
+    states[0]['t'] = 10;
+    states[0]['u'] = 10;
+    states[0]['v'] = 10;
+    states[0]['w'] = 10;
+    states[0]['x'] = 10;
+    states[0]['y'] = 10;
+    states[0]['z'] = 10;
+    states[0]['.'] = 10;
+    states[0][':'] = 2;
+
+    states[1]['0'] = 1;
+    states[1]['1'] = 1;
+    states[1]['2'] = 1;
+    states[1]['3'] = 1;
+    states[1]['4'] = 1;
+    states[1]['5'] = 1;
+    states[1]['6'] = 1;
+    states[1]['7'] = 1;
+    states[1]['8'] = 1;
+    states[1]['9'] = 1;
+    states[1]['a'] = 1;
+    states[1]['b'] = 1;
+    states[1]['c'] = 1;
+    states[1]['d'] = 1;
+    states[1]['e'] = 1;
+    states[1]['f'] = 1;
+    states[1][':'] = 2;
+
+    states[3]['f'] = 4;
+    states[3]['p'] = 7;
+    states[3]['s'] = 6;
+    states[3]['w'] = 5;
+    states[3]['x'] = 5;
+    states[3]['l'] = 11;
+
+    states[4]['0'] = 1;
+    states[4]['1'] = 1;
+    states[4]['2'] = 1;
+    states[4]['3'] = 1;
+    states[4]['4'] = 1;
+    states[4]['5'] = 1;
+    states[4]['6'] = 1;
+    states[4]['7'] = 1;
+    states[4]['8'] = 1;
+    states[4]['9'] = 1;
+    states[4]['a'] = 0;
+    states[4]['b'] = 0;
+    states[4]['c'] = 0;
+    states[4]['d'] = 0;
+    states[4]['e'] = 0;
+    states[4]['f'] = 0;
+    states[4]['g'] = 10;
+    states[4]['h'] = 10;
+    states[4]['i'] = 10;
+    states[4]['j'] = 10;
+    states[4]['k'] = 10;
+    states[4]['l'] = 10;
+    states[4]['m'] = 10;
+    states[4]['n'] = 10;
+    states[4]['o'] = 10;
+    states[4]['p'] = 9;
+    states[4]['q'] = 10;
+    states[4]['r'] = 10;
+    states[4]['s'] = 10;
+    states[4]['t'] = 10;
+    states[4]['u'] = 10;
+    states[4]['v'] = 10;
+    states[4]['w'] = 10;
+    states[4]['x'] = 10;
+    states[4]['y'] = 10;
+    states[4]['z'] = 10;
+    states[4][':'] = 2;
+
+    states[5]['0'] = 8;
+    states[5]['1'] = 8;
+    states[5]['2'] = 8;
+    states[5]['3'] = 8;
+    states[5]['4'] = 8;
+    states[5]['5'] = 8;
+    states[5]['6'] = 8;
+    states[5]['7'] = 8;
+    states[5]['8'] = 8;
+    states[5]['9'] = 8;
+    states[5]['a'] = 10;
+    states[5]['b'] = 10;
+    states[5]['c'] = 10;
+    states[5]['d'] = 10;
+    states[5]['e'] = 10;
+    states[5]['f'] = 10;
+    states[5]['g'] = 10;
+    states[5]['h'] = 10;
+    states[5]['i'] = 10;
+    states[5]['j'] = 10;
+    states[5]['k'] = 10;
+    states[5]['l'] = 10;
+    states[5]['m'] = 10;
+    states[5]['n'] = 10;
+    states[5]['o'] = 10;
+    states[5]['p'] = 10;
+    states[5]['q'] = 10;
+    states[5]['r'] = 10;
+    states[5]['s'] = 10;
+    states[5]['t'] = 10;
+    states[5]['u'] = 10;
+    states[5]['v'] = 10;
+    states[5]['w'] = 10;
+    states[5]['x'] = 10;
+    states[5]['y'] = 10;
+    states[5]['z'] = 10;
+
+    states[6]['a'] = 10;
+    states[6]['b'] = 10;
+    states[6]['c'] = 10;
+    states[6]['d'] = 10;
+    states[6]['e'] = 10;
+    states[6]['f'] = 10;
+    states[6]['g'] = 10;
+    states[6]['h'] = 10;
+    states[6]['i'] = 10;
+    states[6]['j'] = 10;
+    states[6]['k'] = 10;
+    states[6]['l'] = 10;
+    states[6]['m'] = 10;
+    states[6]['n'] = 10;
+    states[6]['o'] = 10;
+    states[6]['p'] = 9;
+    states[6]['q'] = 10;
+    states[6]['r'] = 10;
+    states[6]['s'] = 10;
+    states[6]['t'] = 10;
+    states[6]['u'] = 10;
+    states[6]['v'] = 10;
+    states[6]['w'] = 10;
+    states[6]['x'] = 10;
+    states[6]['y'] = 10;
+    states[6]['z'] = 10;
+
+    states[7]['a'] = 10;
+    states[7]['b'] = 10;
+    states[7]['c'] = 9;
+    states[7]['d'] = 10;
+    states[7]['e'] = 10;
+    states[7]['f'] = 10;
+    states[7]['g'] = 10;
+    states[7]['h'] = 10;
+    states[7]['i'] = 10;
+    states[7]['j'] = 10;
+    states[7]['k'] = 10;
+    states[7]['l'] = 10;
+    states[7]['m'] = 10;
+    states[7]['n'] = 10;
+    states[7]['o'] = 10;
+    states[7]['p'] = 10;
+    states[7]['q'] = 10;
+    states[7]['r'] = 10;
+    states[7]['s'] = 10;
+    states[7]['t'] = 10;
+    states[7]['u'] = 10;
+    states[7]['v'] = 10;
+    states[7]['w'] = 10;
+    states[7]['x'] = 10;
+    states[7]['y'] = 10;
+    states[7]['z'] = 10;
+
+    states[8]['0'] = 9;
+    states[8]['1'] = 9;
+    states[8]['2'] = 9;
+    states[8]['3'] = 9;
+    states[8]['4'] = 9;
+    states[8]['5'] = 9;
+    states[8]['6'] = 9;
+    states[8]['7'] = 9;
+    states[8]['8'] = 9;
+    states[8]['9'] = 9;
+
+    states[10]['a'] = 10;
+    states[10]['b'] = 10;
+    states[10]['c'] = 10;
+    states[10]['d'] = 10;
+    states[10]['e'] = 10;
+    states[10]['f'] = 10;
+    states[10]['g'] = 10;
+    states[10]['h'] = 10;
+    states[10]['i'] = 10;
+    states[10]['j'] = 10;
+    states[10]['k'] = 10;
+    states[10]['l'] = 10;
+    states[10]['m'] = 10;
+    states[10]['n'] = 10;
+    states[10]['o'] = 10;
+    states[10]['p'] = 10;
+    states[10]['q'] = 10;
+    states[10]['r'] = 10;
+    states[10]['s'] = 10;
+    states[10]['t'] = 10;
+    states[10]['u'] = 10;
+    states[10]['v'] = 10;
+    states[10]['w'] = 10;
+    states[10]['x'] = 10;
+    states[10]['y'] = 10;
+    states[10]['z'] = 10;
+
+    states[11]['a'] = 10;
+    states[11]['b'] = 10;
+    states[11]['c'] = 10;
+    states[11]['d'] = 10;
+    states[11]['e'] = 10;
+    states[11]['f'] = 10;
+    states[11]['g'] = 10;
+    states[11]['h'] = 10;
+    states[11]['i'] = 10;
+    states[11]['j'] = 10;
+    states[11]['k'] = 10;
+    states[11]['l'] = 10;
+    states[11]['m'] = 10;
+    states[11]['n'] = 10;
+    states[11]['o'] = 10;
+    states[11]['p'] = 10;
+    states[11]['q'] = 10;
+    states[11]['r'] = 9;
+    states[11]['s'] = 10;
+    states[11]['t'] = 10;
+    states[11]['u'] = 10;
+    states[11]['v'] = 10;
+    states[11]['w'] = 10;
+    states[11]['x'] = 10;
+    states[11]['y'] = 10;
+    states[11]['z'] = 10;
+
+    int i;
+    for(i = 0; i < states_count; ++i)
+    {
+        automata.push(states[i]);
+    }
+    automata.add_start_state(3);
     automata.add_start_state(0);
-    automata.add_accept(std::make_pair(0, HEX_NUMBER));
-    automata.add_accept(std::make_pair(1, ADDRESS));
+    automata.add_accept(std::make_pair(0, HEX_OR_INST));
+    automata.add_accept(std::make_pair(1, HEX_NUMBER));
+    automata.add_accept(std::make_pair(2, ADDRESS));
+    automata.add_accept(std::make_pair(4, HEX_NUMBER));
+    automata.add_accept(std::make_pair(8, REGISTER));
+    automata.add_accept(std::make_pair(9, REGISTER));
+    automata.add_accept(std::make_pair(10, INSTRUCTION));
 }
 
 void init_register_automata()
@@ -98,7 +350,7 @@ char* type_to_str(int a)
     switch(a)
     {
         case ADDRESS:
-            return (char*)"address";
+            return (char*)"address of instruction";
         case HEX_NUMBER:
             return (char*)"hex number";
         case REGISTER:
@@ -109,6 +361,10 @@ char* type_to_str(int a)
             return (char*)"offset";
         case MEMORY_ACCESS:
             return (char*)"memory access";
+        case HEX_OR_INST:
+            return (char*)"hex or instruction";
+        case INSTRUCTION:
+            return (char*)"instruction";
         default:
             print_fatal("Type unknown\n");
     }
@@ -124,7 +380,7 @@ void process_line(char* line)
     while(it < len && (line[it] == ' ' || line[it] == '\t' || line[it] == '\n')) it++;
     while(it < len)
     {
-        if(line[it] == ' ' || line[it] == '\n' || line[it] == '\t')
+        if(line[it] == ' ' || line[it] == '\n' || line[it] == '\t' || line[it] == ',')
         {
             buf[buf_idx] = '\0';
             int status = automata.get_accept();
